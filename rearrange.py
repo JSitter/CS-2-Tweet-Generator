402e70487efb835d7bec6aff3c3322dd8cdb3f92
Justin Sitter <justin@jaytria.com>
2017-10-24 18:18:55 -0700

    removed debugging code

--- rearrange.py
+++ rearrange.py
@@ -3,12 +3,10 @@ from random import randint
 
 #create a set
 words = set()
-index = 0
 
-while (len(words) != len(sys.argv)-1) and index<20:
+
+while (len(words) != len(sys.argv)-1):
     randomNum = randint(1, len(sys.argv)-1)
     words.add(sys.argv[randomNum])
-    index += 1
-
 
 print(" ".join(words))
