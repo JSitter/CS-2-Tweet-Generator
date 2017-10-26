18f21da8e17599271e63512b690791ea1f52a1ca
Justin Sitter <justin@jaytria.com>
2017-10-25 22:33:35 -0700

    minor tweaks

--- dictionary_words.py
+++ dictionary_words.py
@@ -3,25 +3,37 @@ import random
 
 def create_sentence(num_of_words):
     '''Create a sentence given number of words in sentence'''
+    #Open dictionary
     with open('american') as f:
         word_list = list(f)
     
         word_ct = 0 #Words generated
         sentence = ""
 
-        while words < sentence_length:
-            words += 1
-            rand_int = random.randint(0, len(word_list))
-            random_word = word_list[rand_int]
-            #uppercase first letter of first word
-            # if words == 1:
-            #     sentence += random_word
+        while word_ct < sentence_length:
+            word_ct += 1
 
-            sentence += word_list[rand_int] + " "
+            #Get random number and subract 1 to prevent out of bounds error
+            rand_int = random.randint(0, len(word_list)) - 1 
+            random_word = word_list[rand_int]
+
+            #lowercase and remove newlines from input
+            random_word = random_word.strip("\n").lower()
+            
+            #uppercase first letter of first word
+            if word_ct == 1:
+                sentence += random_word[0].upper() + random_word[2:]
+            #Add Middle Words
+            sentence += random_word + " "
+            
+            #Replace last space with period on last iteration
+            if word_ct == sentence_length:
+                sentence = sentence[:-1] + "."
+
         return sentence
 
 #Call function and pass in parameters
-if __name__= "__main__":
+if __name__=="__main__":
     sentence_length = int(sys.argv[1])
 
     print(create_sentence(sentence_length))
