3a13ca0ed5db00b7ef8fa47a145061bbff330b58
Justin Sitter <justin@jaytria.com>
2017-10-25 23:01:19 -0700

    fixed test name

--- test_dictionary_words.py
+++ test_dictionary_words.py
@@ -2,7 +2,7 @@ import pytest
 import time
 import dictionary_words as rand_words
 
-def test_sentence_word len():
+def test_sentence_word_len():
     start_time = int(round(time.time()*1000))
     sentence = rand_words.create_sentence(10)
     end_time = int(round(time.time()*1000))
