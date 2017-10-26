0d71a4e6011f33705e2b0adc7edfde54ecba4a9c
Justin Sitter <justin@jaytria.com>
2017-10-25 22:56:45 -0700

    added test and timing

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import time
 
 def create_sentence(num_of_words):
     '''Create a sentence given number of words in sentence'''
@@ -10,7 +11,7 @@ def create_sentence(num_of_words):
         word_ct = 0 #Words generated
         sentence = ""
 
-        while word_ct < sentence_length:
+        while word_ct < num_of_words:
             word_ct += 1
 
             #Get random number and subract 1 to prevent out of bounds error
@@ -19,7 +20,7 @@ def create_sentence(num_of_words):
 
             #lowercase and remove newlines from input
             random_word = random_word.strip("\n").lower()
-            
+
             #uppercase first letter of first word
             if word_ct == 1:
                 sentence += random_word[0].upper() + random_word[2:]
@@ -27,7 +28,7 @@ def create_sentence(num_of_words):
             sentence += random_word + " "
             
             #Replace last space with period on last iteration
-            if word_ct == sentence_length:
+            if word_ct == num_of_words:
                 sentence = sentence[:-1] + "."
 
         return sentence
@@ -35,5 +36,10 @@ def create_sentence(num_of_words):
 #Call function and pass in parameters
 if __name__=="__main__":
     sentence_length = int(sys.argv[1])
+    start_time = int(round(time.time()*1000))
+    sentence = create_sentence(sentence_length)
+    end_time = int(round(time.time()*1000))
 
-    print(create_sentence(sentence_length))
+    print(sentence)
+    print("\n{} word sentence generated in {}ms.".format(sentence_length, end_time-start_time))
+    print("This product provided by Jaytria Industries - Where people become dolphins.")
---
+++ test_dictionary_words.py
@@ -0,0 +1,11 @@
+import pytest
+import time
+import dictionary_words as rand_words
+
+def test_sentence_word len():
+    start_time = int(round(time.time()*1000))
+    sentence = rand_words.create_sentence(10)
+    end_time = int(round(time.time()*1000))
+
+    print(end_time - start_time)
+    assert len(sentence.split(' ')) == 10
