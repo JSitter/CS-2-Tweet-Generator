406a4fa68481528660a4c0cf4599f8bfb7543885
Justin Sitter <justin@jaytria.com>
2017-10-25 22:03:41 -0700

    basic create sentence function

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,15 +1,27 @@
 import sys
 import random
 
-
-with open('american') as f:
-    word_list = list(f)
-
-
-sentence_length = int(sys.argv[1])
-words = 0
-
-while words < sentence_length:
-    words += 1
-    rand_int = random.randint(0, len(word_list))
+def create_sentence(num_of_words):
+    '''Create a sentence given number of words in sentence'''
+    with open('american') as f:
+        word_list = list(f)
     
+        word_ct = 0 #Words generated
+        sentence = ""
+
+        while words < sentence_length:
+            words += 1
+            rand_int = random.randint(0, len(word_list))
+            random_word = word_list[rand_int]
+            #uppercase first letter of first word
+            # if words == 1:
+            #     sentence += random_word
+
+            sentence += word_list[rand_int] + " "
+        return sentence
+
+#Call function and pass in parameters
+if __name__= "__main__":
+    sentence_length = int(sys.argv[1])
+
+    print(create_sentence(sentence_length))
--- rearrange.py
+++ rearrange.py
@@ -4,7 +4,6 @@ from random import randint
 #create a set
 words = set()
 
-
 while (len(words) != len(sys.argv)-1):
     randomNum = randint(1, len(sys.argv)-1)
     words.add(sys.argv[randomNum])
