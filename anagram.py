0e3e0aa826fdf8b570aa330380dfce76c161bcf0
Justin Sitter <justin@jaytria.com>
2017-10-26 13:45:31 -0700

    creates key for word

--- anagram.py
+++ anagram.py
@@ -1,9 +1,28 @@
 import sys
 from random import randint
 import time
+import collections
 
 def create_anagram(word):
-    pass
+    with open("american") as f:
+        word_list = list(f)
+        anagram_dict = dict()
+        #hashy_dict = collections.defaultdict(list)
+        #letter_hashy = set(collections.Counter())
+
+        # for c in word_list[763]:
+        #     letter_hashy.add(c)
+        # print(word_list[763])
+        # print(letter_hashy)
+        cleaned_word = word_list[763].strip("\n")
+
+        charlist = collections.Counter(list(cleaned_word))
+        wordkey = ""
+        for ch in charlist.most_common():
+            wordkey += ch[0] + str(ch[1])
+
+        print(wordkey)
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
