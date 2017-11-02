fd10e58a78d9725ab7249bd6da15d8fd2bcfedf6
Justin Sitter <justin@jaytria.com>
2017-11-02 15:28:12 -0700

    Weighted random sampling works

--- stochastic_sample.py
+++ stochastic_sample.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 import sys
+from random import random
+import time
 
 def stochastic_sample(filename, sentence_length):
     words = get_words_from_text(filename)
-    #num_words = len(words)
-    create_histogram(words)
-    print(len(words))
+    histogram = create_histogram(words)
+    total_words = len(words)
+    place_holder = 0
 
+    
+
+    for item in histogram:
+        place_holder += histogram[item]/total_words
+        histogram[item] = place_holder
+        
+    count = 0
+    sentence = ""
+    for count in range(sentence_length):
+        random_number = random()
+        
+        for item in histogram:
+            
+            if  random_number <= histogram[item]:
+                sentence = sentence + item + " "
+                break
+    print(sentence)
+
+ 
 
 def create_histogram(word_list):
     '''
@@ -38,7 +59,7 @@ def sanitize(word):
     if word == "\n":
         word = "*filtered*"
     
-    word = word.strip(".,:\n").lower()
+    word = word.strip(". ,:\r\n\"\t!").lower()
     
     return word
 
@@ -51,8 +72,10 @@ if __name__ == "__main__":
         filename = default_file_name
     
     if len(sys.argv) > 2:
-        sentence_len = sys.argv[2]
+        sentence_len = int(sys.argv[2])
     else:
         sentence_len = 5
-
+    start_time = int(round(time.time()*1000))
     stochastic_sample(filename, sentence_len)
+    end_time = int(round(time.time()*1000))
+    print("Ran in {}ms".format(end_time - start_time))
