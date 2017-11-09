a6da3a6a30ae9db6b1c965b1beff717270e1a2a7
Justin Sitter <justin@jaytria.com>
2017-11-09 10:57:15 -0800

    Added class and method framework

---
+++ StochasticSampling.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import sys
+from random import random
+import time
+
+class StochasticSample:
+
+    def __init__(filename, sentence_lenth):
+        self.sourceText = 
+
+    def createHistogram(word_list):
+        '''
+            Create Histogram of word freqency
+        '''
+        pass
+    
+    def getWordsFromText(filename):
+        '''
+            Get words from Text File
+        '''
+        pass
+    
+    def santizeText(word_list):
+        '''
+            Santize words
+        '''
+        pass
+    
+
+if __name__=="__main__":
+    defaultFileName = "small_sample_text.txt"
+
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = default_file_name
+    
+    if len(sys.argv) > 2:
+        sentence_len = int(sys.argv[2])
+    else:
+        sentence_len = 5
+    start_time = int(round(time.time()*1000))
+    #Run sampling here
+    
+    end_time = int(round(time.time()*1000))
+    print("Ran in {}ms".format(end_time - start_time))
